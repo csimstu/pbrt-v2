ad0470ed9c75bc869c29ca38d02f048cd9765e80
csimstu <csimstu@DN51q05n.SUNet>
2016-04-15 20:23:15 -0700

    Finish step 3

--- src/shapes/distanceestimator.cpp
+++ src/shapes/distanceestimator.cpp
@@ -1,186 +1,85 @@
 #include "shapes/distanceestimator.h"
+#include "geometry.h"
 #include "paramset.h"
 
-DistanceEstimator::DistanceEstimator(const Transform *o2w, const Transform *w2o, bool ro,
-               float rad, float z0, float z1, float pm)
+DistanceEstimator::DistanceEstimator(const Transform *o2w, const Transform *w2o, bool ro, float rad)
     : Shape(o2w, w2o, ro) {
     radius = rad;
-    zmin = Clamp(min(z0, z1), -radius, radius);
-    zmax = Clamp(max(z0, z1), -radius, radius);
-    thetaMin = acosf(Clamp(zmin/radius, -1.f, 1.f));
-    thetaMax = acosf(Clamp(zmax/radius, -1.f, 1.f));
-    phiMax = Radians(Clamp(pm, 0.0f, 360.0f));
+    maxIters = 1000;
+    hitEpsilon = 1e-6f;
+    rayEpsilonMultiplier = 2.0f;
+    normalEpsilon = 1e-4f;
 }
 
 BBox DistanceEstimator::ObjectBound() const {
-    return BBox(Point(-radius, -radius, zmin),
-                Point( radius,  radius, zmax));
+    return BBox(Point(-radius, -radius, -radius),
+                Point( radius,  radius, radius));
 }
 
 bool DistanceEstimator::Intersect(const Ray &r, float *tHit, float *rayEpsilon,
                        DifferentialGeometry *dg) const {
-    float phi;
-    Point phit;
-    // Transform _Ray_ to object space
-    Ray ray;
-    (*WorldToObject)(r, &ray);
+  bool succeed = DoesIntersect(r, tHit);
+  if (!succeed) return false;
 
-    // Compute quadratic sphere coefficients
-    float A = ray.d.x*ray.d.x + ray.d.y*ray.d.y + ray.d.z*ray.d.z;
-    float B = 2 * (ray.d.x*ray.o.x + ray.d.y*ray.o.y + ray.d.z*ray.o.z);
-    float C = ray.o.x*ray.o.x + ray.o.y*ray.o.y +
-              ray.o.z*ray.o.z - radius*radius;
-
-    // Solve quadratic equation for _t_ values
-    float t0, t1;
-    if (!Quadratic(A, B, C, &t0, &t1))
-        return false;
-
-    // Compute intersection distance along ray
-    if (t0 > ray.maxt || t1 < ray.mint)
-        return false;
-    float thit = t0;
-    if (t0 < ray.mint) {
-        thit = t1;
-        if (thit > ray.maxt) return false;
-    }
-
-    // Compute sphere hit position and $\phi$
-    phit = ray(thit);
-    if (phit.x == 0.f && phit.y == 0.f) phit.x = 1e-5f * radius;
-    phi = atan2f(phit.y, phit.x);
-    if (phi < 0.) phi += 2.f*M_PI;
-
-    // Test sphere intersection against clipping parameters
-    if ((zmin > -radius && phit.z < zmin) ||
-        (zmax <  radius && phit.z > zmax) || phi > phiMax) {
-        if (thit == t1) return false;
-        if (t1 > ray.maxt) return false;
-        thit = t1;
-        // Compute sphere hit position and $\phi$
-        phit = ray(thit);
-        if (phit.x == 0.f && phit.y == 0.f) phit.x = 1e-5f * radius;
-        phi = atan2f(phit.y, phit.x);
-        if (phi < 0.) phi += 2.f*M_PI;
-        if ((zmin > -radius && phit.z < zmin) ||
-            (zmax <  radius && phit.z > zmax) || phi > phiMax)
-            return false;
-    }
-
-    // Find parametric representation of sphere hit
-    float u = phi / phiMax;
-    float theta = acosf(Clamp(phit.z / radius, -1.f, 1.f));
-    float v = (theta - thetaMin) / (thetaMax - thetaMin);
-
-    // Compute sphere $\dpdu$ and $\dpdv$
-    float zradius = sqrtf(phit.x*phit.x + phit.y*phit.y);
-    float invzradius = 1.f / zradius;
-    float cosphi = phit.x * invzradius;
-    float sinphi = phit.y * invzradius;
-    Vector dpdu(-phiMax * phit.y, phiMax * phit.x, 0);
-    Vector dpdv = (thetaMax-thetaMin) *
-        Vector(phit.z * cosphi, phit.z * sinphi,
-               -radius * sinf(theta));
-
-    // Compute sphere $\dndu$ and $\dndv$
-    Vector d2Pduu = -phiMax * phiMax * Vector(phit.x, phit.y, 0);
-    Vector d2Pduv = (thetaMax - thetaMin) * phit.z * phiMax *
-                    Vector(-sinphi, cosphi, 0.);
-    Vector d2Pdvv = -(thetaMax - thetaMin) * (thetaMax - thetaMin) *
-                    Vector(phit.x, phit.y, phit.z);
-
-    // Compute coefficients for fundamental forms
-    float E = Dot(dpdu, dpdu);
-    float F = Dot(dpdu, dpdv);
-    float G = Dot(dpdv, dpdv);
-    Vector N = Normalize(Cross(dpdu, dpdv));
-    float e = Dot(N, d2Pduu);
-    float f = Dot(N, d2Pduv);
-    float g = Dot(N, d2Pdvv);
-
-    // Compute $\dndu$ and $\dndv$ from fundamental form coefficients
-    float invEGF2 = 1.f / (E*G - F*F);
-    Normal dndu = Normal((f*F - e*G) * invEGF2 * dpdu +
-                         (e*F - f*E) * invEGF2 * dpdv);
-    Normal dndv = Normal((g*F - f*G) * invEGF2 * dpdu +
-                         (f*F - g*E) * invEGF2 * dpdv);
-
-    // Initialize _DifferentialGeometry_ from parametric information
-    const Transform &o2w = *ObjectToWorld;
-    *dg = DifferentialGeometry(o2w(phit), o2w(dpdu), o2w(dpdv),
-                               o2w(dndu), o2w(dndv), u, v, this);
-
-    // Update _tHit_ for quadric intersection
-    *tHit = thit;
-
-    // Compute _rayEpsilon_ for quadric intersection
-    *rayEpsilon = 5e-4f * *tHit;
-    return true;
+  Ray ray;
+  (*WorldToObject)(r, &ray);
+  Point p = ray(*tHit);
+  *rayEpsilon = hitEpsilon * rayEpsilonMultiplier;
+  Vector n = CalculateNormal(p, normalEpsilon);
+  Vector DPDU, DPDV;
+  CoordinateSystem(n, &DPDU, &DPDV);
+  *dg = DifferentialGeometry(p, DPDU, DPDV, Normal(), Normal(), 0, 0, this);
+  return true;
 }
 
 
 bool DistanceEstimator::IntersectP(const Ray &r) const {
-    float phi;
-    Point phit;
-    // Transform _Ray_ to object space
-    Ray ray;
-    (*WorldToObject)(r, &ray);
-
-    // Compute quadratic sphere coefficients
-    float A = ray.d.x*ray.d.x + ray.d.y*ray.d.y + ray.d.z*ray.d.z;
-    float B = 2 * (ray.d.x*ray.o.x + ray.d.y*ray.o.y + ray.d.z*ray.o.z);
-    float C = ray.o.x*ray.o.x + ray.o.y*ray.o.y +
-              ray.o.z*ray.o.z - radius*radius;
-
-    // Solve quadratic equation for _t_ values
-    float t0, t1;
-    if (!Quadratic(A, B, C, &t0, &t1))
-        return false;
-
-    // Compute intersection distance along ray
-    if (t0 > ray.maxt || t1 < ray.mint)
-        return false;
-    float thit = t0;
-    if (t0 < ray.mint) {
-        thit = t1;
-        if (thit > ray.maxt) return false;
-    }
-
-    // Compute sphere hit position and $\phi$
-    phit = ray(thit);
-    if (phit.x == 0.f && phit.y == 0.f) phit.x = 1e-5f * radius;
-    phi = atan2f(phit.y, phit.x);
-    if (phi < 0.) phi += 2.f*M_PI;
-
-    // Test sphere intersection against clipping parameters
-    if ((zmin > -radius && phit.z < zmin) ||
-        (zmax <  radius && phit.z > zmax) || phi > phiMax) {
-        if (thit == t1) return false;
-        if (t1 > ray.maxt) return false;
-        thit = t1;
-        // Compute sphere hit position and $\phi$
-        phit = ray(thit);
-        if (phit.x == 0.f && phit.y == 0.f) phit.x = 1e-5f * radius;
-        phi = atan2f(phit.y, phit.x);
-        if (phi < 0.) phi += 2.f*M_PI;
-        if ((zmin > -radius && phit.z < zmin) ||
-            (zmax <  radius && phit.z > zmax) || phi > phiMax)
-            return false;
-    }
-    return true;
+  return DoesIntersect(r, NULL);
 }
 
 
 float DistanceEstimator::Area() const {
-    return phiMax * radius * (zmax-zmin);
+    return 4 * M_PI * radius * radius;
 }
 
 DistanceEstimator *CreateDistanceEstimatorShape(const Transform *o2w, const Transform *w2o,
         bool reverseOrientation, const ParamSet &params) {
     float radius = params.FindOneFloat("radius", 1.f);
-    float zmin = params.FindOneFloat("zmin", -radius);
-    float zmax = params.FindOneFloat("zmax", radius);
-    float phimax = params.FindOneFloat("phimax", 360.f);
-    return new DistanceEstimator(o2w, w2o, reverseOrientation, radius,
-                      zmin, zmax, phimax);
+    return new DistanceEstimator(o2w, w2o, reverseOrientation, radius);
+}
+
+float DistanceEstimator::Evaluate(const Point &p) const {
+  return sqrt(p.x * p.x + p.y * p.y + p.z * p.z) - radius;
+}
+
+Vector DistanceEstimator::CalculateNormal(const Point& pos, float eps) const {
+  const Vector v1 = Vector( 1.0,-1.0,-1.0);
+  const Vector v2 = Vector(-1.0,-1.0, 1.0);
+  const Vector v3 = Vector(-1.0, 1.0,-1.0);
+  const Vector v4 = Vector( 1.0, 1.0, 1.0);
+
+  return Normalize( 
+      v1 * Evaluate( pos + v1*eps ) +
+      v2 * Evaluate( pos + v2*eps ) +
+      v3 * Evaluate( pos + v3*eps ) +
+      v4 * Evaluate( pos + v4*eps ) );
+}
+
+bool DistanceEstimator::DoesIntersect(const Ray &r, float *tHit) const {
+  Ray ray;
+  (*WorldToObject)(r, &ray);
+  float t = Evaluate(ray(ray.mint));
+  bool intersected = false;
+  for (int itr = 0; ray.mint <= t && t <= ray.maxt && itr < maxIters; itr++) {
+    float dist = Evaluate(ray(t));
+    if (fabs(dist) < hitEpsilon) {
+      intersected = true;
+      break;
+    }
+    t += dist;
+  }
+  if (!intersected) 
+    return false;
+  if (tHit) *tHit = t;
+  return true;
 }
--- src/shapes/distanceestimator.h
+++ src/shapes/distanceestimator.h
@@ -10,22 +10,27 @@
 class DistanceEstimator: public Shape {
   public:
     // Shape Interface
-    DistanceEstimator (const Transform *o2w, const Transform *w2o, bool ro, float rad,
-           float zmin, float zmax, float phiMax);
+    DistanceEstimator (const Transform *o2w, const Transform *w2o, bool ro, float rad);
     BBox ObjectBound() const;
     bool Intersect(const Ray &ray, float *tHit,
         float *rayEpsilon, DifferentialGeometry *dg) const;
     bool IntersectP(const Ray &ray) const;
     float Area() const;
+
+    virtual float Evaluate(const Point &p) const;
 private:
+    Vector CalculateNormal(const Point& pos, float eps) const;
+    bool DoesIntersect(const Ray &r, float *tHit) const;
+
     // Sphere Private Data
     float radius;
-    float phiMax;
-    float zmin, zmax;
-    float thetaMin, thetaMax;
+    int maxIters; // Number of steps along the ray until we give up (default 1000)
+    float hitEpsilon; // how close to the surface we must be before we say we "hit" it 
+    float rayEpsilonMultiplier; // how much we multiply hitEpsilon by to get rayEpsilon 
+    float normalEpsilon; // The epsilon we send to CalculateNormal()
 };
 
 DistanceEstimator *CreateDistanceEstimatorShape(const Transform *o2w, const Transform *w2o,
-        bool reverseOrientation, const ParamSet &params);
+    bool reverseOrientation, const ParamSet &params);
 
 #endif // PBRT_SHAPES_DISTANCE_ESTIMATOR_H
